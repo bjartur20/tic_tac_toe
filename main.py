ecfb2c4f219ca724af142d5fdd307453c4ef95cd
bjartur20 <bjartur20@gmail.com>
2023-11-17 23:55:12 +0000

    detect tie and reset board

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 class GameBoard:
-    def __init__(self):
+    def setup_game_board(self):
         self.cells = [
             [" ", " ", " "],
             [" ", " ", " "],
             [" ", " ", " "],
         ]
 
-    def __str__(self):
+    def __str__(self) -> str:
         ret = f"{'-' * len(self.cells*2)}-\n"
 
         for row in self.cells:
@@ -14,6 +14,10 @@ class GameBoard:
             ret += f"{'-' * len(self.cells*2)}-\n"
 
         return ret
+
+    @property
+    def is_full(self) -> bool:
+        return all([cell != " " for row in self.cells for cell in row])
 
     def place_symbol(self, symbol: str, place: int):
         row = place // 3
@@ -38,12 +42,14 @@ class Game:
     def start(self):
         play_again = True
         while play_again:
+            self.game_board.setup_game_board()
             self.__game_loop()
             play_again = self.__play_again()
 
     def __game_loop(self):
+        game_running = True
         current_turn = self.player_x
-        while True:
+        while game_running:
             print(self.game_board)
             if current_turn == self.player_x:
                 self.player_x.take_turn(self.game_board)
@@ -52,10 +58,20 @@ class Game:
                 self.player_o.take_turn(self.game_board)
                 current_turn = self.player_x
 
+            game_running = not self.__is_game_finished()
+
+    def __is_game_finished(self) -> bool:
+        if self.game_board.is_full:
+            print("Tie!")
+            return True
+
+        return False
+
+
     @staticmethod
-    def __play_again(self):
+    def __play_again() -> bool:
         response = input("Would you like to play again? (y): ")
-        return True if response == "y" else False
+        return response == "y"
 
 def main():
     Game().start()
