2cd1ffc1629fc9f3f1c42b86a139064aef638019
bjartur20 <bjartur20@gmail.com>
2023-11-18 15:35:23 +0000

    extract input validation into a seperate function

--- src/player.py
+++ src/player.py
@@ -11,16 +11,22 @@ class Player:
         while invalid_input:
             response = get_input("Select a square (1-9): ")
 
-            if not response.isdigit():
-                print(f"Please input a number")
-                continue
-            if not (1 <= int(response) <= 9):
-                print(f"Please input a number between 1 and 9")
-                continue
-            if not game_board.is_cell_empty(int(response) - 1):
-                print(f"Cell at position {response} has already been played")
+            if not self.__validate_input(response, game_board):
                 continue
 
             invalid_input = False
         
         game_board.place_symbol(self.symbol, int(response)-1)
+
+    def __validate_input(self, input_value: str, game_board: GameBoard) -> bool:
+        if not input_value.isdigit():
+            print(f"Please input a number")
+            return False
+        if not (1 <= int(input_value) <= 9):
+            print(f"Please input a number between 1 and 9")
+            return False
+        if not game_board.is_cell_empty(int(input_value) - 1):
+            print(f"Cell at position {input_value} has already been played")
+            return False
+
+        return True
