1c4fac9d841f894f2634e089e14bcf8989a5ffe9
bjartur20 <bjartur20@gmail.com>
2023-11-18 02:27:09 +0000

    testing for Player class implemented

--- src/player.py
+++ src/player.py
@@ -1,4 +1,5 @@
 from src.board import GameBoard
+from src.utils import get_input
 
 class Player:
     def __init__(self, symbol: str):
@@ -7,7 +8,7 @@ class Player:
     def take_turn(self, game_board: GameBoard):
         invalid_input = True
         while invalid_input:
-            response = input("Select a square (1-9): ")
+            response = get_input("Select a square (1-9): ")
             if response.isdigit() and (1 <= int(response) <= 9):
                 invalid_input = False
             print("Please input a number between 1 and 9")
