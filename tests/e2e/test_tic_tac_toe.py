8af82a80d551c5e25897bd6d0ce4f218a5964f22
bjartur20 <bjartur20@gmail.com>
2023-11-18 16:02:41 +0000

    e2e tests for multiple games added

--- tests/e2e/test_tic_tac_toe.py
+++ tests/e2e/test_tic_tac_toe.py
@@ -8,8 +8,8 @@ from src.player import Player
 
 @mock.patch("src.game.get_input")
 @mock.patch("src.player.get_input")
-def test_valites_x_win_correctly(mock_player_input, mock_game_input, game: Game):
-    mock_player_input.side_effect = ["1", "2", "4", "3", "7"]
+def test_valites_x_win_correctly(mock_player_input, mock_game_input, game: Game, player_x_winning_sequence: list[str]):
+    mock_player_input.side_effect = player_x_winning_sequence
     mock_game_input.return_value = "n"
     
     game.start()
@@ -25,8 +25,8 @@ def test_valites_x_win_correctly(mock_player_input, mock_game_input, game: Game)
 
 @mock.patch("src.game.get_input")
 @mock.patch("src.player.get_input")
-def test_valites_o_win_correctly(mock_player_input, mock_game_input, game: Game):
-    mock_player_input.side_effect = ["1", "2", "3", "5", "4", "8"]
+def test_valites_o_win_correctly(mock_player_input, mock_game_input, game: Game, player_o_winning_sequence: list[str]):
+    mock_player_input.side_effect = player_o_winning_sequence
     mock_game_input.return_value = "n"
     
     game.start()
@@ -42,8 +42,8 @@ def test_valites_o_win_correctly(mock_player_input, mock_game_input, game: Game)
 
 @mock.patch("src.game.get_input")
 @mock.patch("src.player.get_input")
-def test_valites_tie_correctly(mock_player_input, mock_game_input, game: Game):
-    mock_player_input.side_effect = ["1", "2", "3", "5", "4", "6", "8", "7", "9"]
+def test_valites_tie_correctly(mock_player_input, mock_game_input, game: Game, tie_sequence: list[str]):
+    mock_player_input.side_effect = tie_sequence
     mock_game_input.return_value = "n"
     
     game.start()
@@ -57,7 +57,63 @@ def test_valites_tie_correctly(mock_player_input, mock_game_input, game: Game):
         ]
     }
 
+@mock.patch("src.game.get_input")
+@mock.patch("src.player.get_input")
+def test_can_play_multiple_games_in_a_session(
+    mock_player_input,
+    mock_game_input,
+    game: Game,
+    player_x_winning_sequence: list[str],
+    player_o_winning_sequence: list[str],
+    tie_sequence: list[str]
+):
+    mock_player_input.side_effect = player_x_winning_sequence + player_o_winning_sequence + tie_sequence
+    mock_game_input.side_effect = ["y", "y", "n"]
+    
+    game.start()
+
+    assert game.game_history == [
+        {
+            "winner": "X",
+            "board": [
+                ["X", "O", "O"],
+                ["X", " ", " "],
+                ["X", " ", " "],
+            ]
+        },
+        {
+            "winner": "O",
+            "board": [
+                ["X", "O", "X"],
+                ["X", "O", " "],
+                [" ", "O", " "],
+            ]
+        },
+        {
+            "winner": "Tie",
+            "board": [
+                ["X", "O", "X"],
+                ["X", "O", "O"],
+                ["O", "X", "X"],
+            ]
+        }
+    ]
+
 
 @pytest.fixture
 def game() -> Game:
     return Game(GameBoard(), [Player(symbol="X"), Player(symbol="O")])
+
+
+@pytest.fixture
+def player_x_winning_sequence() -> list[str]:
+    return ["1", "2", "4", "3", "7"]
+
+@pytest.fixture
+def player_o_winning_sequence() -> list[str]:
+    return ["1", "2", "3", "5", "4", "8"]
+
+@pytest.fixture
+def tie_sequence() -> list[str]:
+    return ["1", "2", "3", "5", "4", "6", "8", "7", "9"]
+
