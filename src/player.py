e19f9a60e88d0fdb2b25e2d86f6a69c89d9e1679
bjartur20 <bjartur20@gmail.com>
2023-11-18 01:59:29 +0000

    setup testing (with pytest dependency) and add instructions to readme

---
+++ src/player.py
@@ -0,0 +1,16 @@
+from src.board import GameBoard
+
+class Player:
+    def __init__(self, symbol: str):
+        self.symbol = symbol
+
+    def take_turn(self, game_board: GameBoard):
+        invalid_input = True
+        while invalid_input:
+            response = input("Select a square (1-9): ")
+            if response.isdigit() and (1 <= int(response) <= 9):
+                invalid_input = False
+            print("Please input a number between 1 and 9")
+        
+
+        game_board.place_symbol(self.symbol, int(response)-1)
---
+++ tests/unit/test_player.py
@@ -0,0 +1,4 @@
+from src.player import Player
+
+def test_take_turn_():
+    assert True
